66cd85741fc3e2e2fac92721d48364b16a0a15dd
Gallja <114298054+AndreaGalliano@users.noreply.github.com>
2023-06-25 12:06:08 +0200

    Update index_disiscrizione.php

--- web app/index_disiscrizione.php
+++ web app/index_disiscrizione.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if (isset($_POST['codice_appello']) && isset($_POST['codice_docente']) && isset($_POST['codice_esame']) && isset($_POST['studente'])) {
-        $connection = pg_connect("host=postgres.favo02.dev port=5432 dbname=unitua user=server password=3*2da@ELNj@DFP"); 
+        $connection = pg_connect("host=postgres.favo02.dev port=5432 dbname=unitua user=server password=123"); 
 
         $query = "CALL unitua.delete_iscritto($1, $2, $3, $4)";
 
