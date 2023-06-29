c2b33b08e6bb1c0740cd58d463cd1d43c5d62652
AndreaGalliano <andrea.galliano2@studenti.unimi.it>
2023-06-29 08:06:15 +0200

    first commit of the day

--- web app/home_seg.php
+++ web app/home_seg.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <title>Unitua: Studente</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <title>Unitua: Segreteria</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
-    <h2>Sei stato redirectato, docente!</h2>
-
     <?php
-        include("check_login.php");
-        echo '<h2>'.$_SESSION['isLogin'].'</h2>';
+        include_once('navbar3.php');
+        include_once("check_login.php");
     ?>
+
+
+
 </body>
 </html>
